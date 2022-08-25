e8860e5ccbc81782715e3ac761fa4e2205e4d544
akashsagar2805 <akashsagar5869@gmail.com>
2022-08-26 03:18:22 +0530

    updated Edit

--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Create Student</title>
+    <title>Edit Student</title>
 </head>
 <body>
-  <h1>Create Student</h1>
+  <h1>Edit Student</h1>
   <form action="{{url('/students/' . $student->id)}}" method="post">
     @method('put')
     @csrf
